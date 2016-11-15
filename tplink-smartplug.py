e3ac662d319b409f6d1aed51be32680580a12984
Mika Korhonen <mika.j.korhonen@gmail.com>
2016-11-15 22:03:44 +0200

    Fix typo and remove trailing whitespace
    Signed-off-by: Mika Korhonen <mika.j.korhonen@gmail.com>

--- tplink-smartplug.py
+++ tplink-smartplug.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
-# 
+#
 # TP-Link Wi-Fi Smart Plug Protocol Client
 # For use with TP-Link HS-100 or HS-110
-#  
+#
 # by Lubomir Stroetmann
-# Copyright 2016 softScheck GmbH 
-# 
+# Copyright 2016 softScheck GmbH
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 #
 import socket
 import argparse
@@ -30,7 +30,7 @@ def validIP(ip):
 		socket.inet_pton(socket.AF_INET, ip)
 	except socket.error:
 		parser.error("Invalid IP Address.")
-	return ip 
+	return ip
 
 # Predefined Smart Plug Commands
 # For a full list of commands, consult tplink_commands.txt
@@ -52,18 +52,18 @@ commands = {'info'     : '{"system":{"get_sysinfo":{}}}',
 def encrypt(string):
 	key = 171
 	result = "\0\0\0\0"
-	for i in string: 
+	for i in string:
 		a = key ^ ord(i)
 		key = a
 		result += chr(a)
 	return result
 
 def decrypt(string):
-	key = 171 
+	key = 171
 	result = ""
-	for i in string: 
+	for i in string:
 		a = key ^ ord(i)
-		key = ord(i) 
+		key = ord(i)
 		result += chr(a)
 	return result
 
@@ -85,15 +85,15 @@ else:
 
 
 
-# Send command and receive reply 
+# Send command and receive reply
 try:
 	sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock_tcp.connect((ip, port))
 	sock_tcp.send(encrypt(cmd))
 	data = sock_tcp.recv(2048)
 	sock_tcp.close()
-	
+
 	print "Sent:     ", cmd
 	print "Received: ", decrypt(data[4:])
 except socket.error:
-	quit("Cound not connect to host " + ip + ":" + str(port))
+	quit("Could not connect to host " + ip + ":" + str(port))
